4e5bdf9bb4253f696d3bde0c2ad273929f1147d3
eswar-7116 <eswardudi06@gmail.com>
2025-05-05 23:02:07 +0530

    Add DB connection code

---
+++ src/db/connectDB.ts
@@ -0,0 +1,23 @@
+import { connect } from 'mongoose';
+
+type Connection = {
+    isConnected?: number
+}
+
+const connection: Connection = {}
+
+export default async function connectDB() {
+    if (connection.isConnected) {
+        console.log('Already connected to the DB');
+        return;
+    }
+
+    try {
+        const db = await connect(process.env.DATABASE_URL || '');
+        connection.isConnected = db.connection.readyState;
+        console.log('Successfully connected to the DB');
+    } catch(error) {
+        console.error('Failed to connect to the DB:', error);
+        process.exit(1);
+    }
+}
